351383e3c48cee6876d0b55e812104ed41f47651
white258387 <2583870024@qq.com>
2023-06-04 04:08:10 +0800

    '添加测试docs文件夹'

---
+++ docs/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import  ReactDom from "react-dom";
+
+const App = () => {
+    return (
+        <div>this is app</div>
+    )
+}
+
+ReactDom.render(<App/>, document.getElementById('root'))
